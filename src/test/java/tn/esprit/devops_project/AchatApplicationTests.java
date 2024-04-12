1c7f0aee3b3c83ab06356290e24e5150a377ab1e
onesBenRhaime <onesrhaime28@gmail.com>
2024-04-12 17:58:08 +0100

    jenkinsfile....

--- src/test/java/tn/esprit/devops_project/AchatApplicationTests.java
+++ src/test/java/tn/esprit/devops_project/AchatApplicationTests.java
@@ -3,10 +3,6 @@ package tn.esprit.devops_project;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import tn.esprit.devops_project.repositories.StockRepository;
-import tn.esprit.devops_project.services.StockServiceImpl;
-
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
@@ -14,10 +10,6 @@ class AchatApplicationTests {
 
     @Test
     void contextLoads() {
-        // Ajoutez une assertion pour vérifier que le contexte se charge correctement
-        //        assertNotNull(applicationContext.getBean(StockServiceImpl.class));
-        //      assertNotNull(applicationContext.getBean(StockRepository.class));
-
         assertTrue(true);
     }
 
--- src/test/java/tn/esprit/devops_project/StockServiceMockTest.java
+++ src/test/java/tn/esprit/devops_project/StockServiceMockTest.java
@@ -1,7 +1,9 @@
 package tn.esprit.devops_project;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -9,83 +11,75 @@ import tn.esprit.devops_project.entities.Stock;
 import tn.esprit.devops_project.repositories.StockRepository;
 import tn.esprit.devops_project.services.StockServiceImpl;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class StockServiceMockTest {
 
-    @Mock
-    private StockRepository stockRepository;
+
 
     @InjectMocks
     private StockServiceImpl stockService;
 
-    @Test
-    void testAddStock() {
-        Stock stockToAdd = new Stock();
-        stockToAdd.setIdStock(1L);
-        stockToAdd.setTitle("Sample Stock");
+    @Mock
+    StockRepository universiteRepository;
 
-        when(stockRepository.save(stockToAdd)).thenReturn(stockToAdd);
+    @InjectMocks
+    StockServiceImpl iUniversiteService;
 
-        Stock savedStock = stockService.addStock(stockToAdd);
+    @BeforeEach
+    public void setup() {
 
-        assertEquals(stockToAdd, savedStock);
-        verify(stockRepository, times(1)).save(stockToAdd);
     }
 
     @Test
-    void testRetrieveStock() {
-        Long stockId = 1L;
-        Stock stock = new Stock();
-        stock.setIdStock(stockId);
-        stock.setTitle("Sample Stock");
+    void testGetUniveristesList() {
+        Stock universite1 = new Stock(9, "ben" ,new HashSet<>());
+        when(universiteRepository.findAll()).thenReturn(Arrays.asList(universite1));
+        List<Stock> universiteList = iUniversiteService.retrieveAllStock();
+        assertEquals(2, universiteList.size());
+        assertEquals("ben", universiteList.get(0).getTitle());
 
-        when(stockRepository.findById(stockId)).thenReturn(Optional.of(stock));
+    }
 
-        Stock retrievedStock = stockService.retrieveStock(stockId);
 
-        assertEquals(stock, retrievedStock);
-        verify(stockRepository, times(1)).findById(stockId);
+    @Test
+    void testGetUniveristeById() {
+        Stock universite = new Stock(10, "george",new HashSet<>());
+        when(universiteRepository.findById(10L)).thenReturn(Optional.of(universite));
+        Stock universiteById = iUniversiteService.retrieveStock(10L);
+        assertNotEquals(null, universiteById);
+        assertEquals("george", universiteById.getIdStock());
     }
 
     @Test
-    void testRetrieveAllStock() {
-        // Create sample Stock objects
-        Stock stock1 = new Stock();
-        stock1.setIdStock(1L);
-        stock1.setTitle("Stock 1");
+    void testGetInvalidUniversiteById() {
+        when(universiteRepository.findById(17L)).thenThrow(new RuntimeException("Stock Not Found with ID"));
 
-        Stock stock2 = new Stock();
-        stock2.setIdStock(2L);
-        stock2.setTitle("Stock 2");
+        Exception exception = assertThrows(RuntimeException.class, () -> {
+            iUniversiteService.retrieveStock(17L);
+        });
 
-        // Create a list of Stock objects
-        List<Stock> stocks = new ArrayList<>();
-        stocks.add(stock1);
-        stocks.add(stock2);
-
-        // Mock the behavior of the StockRepository's findAll method
-        when(stockRepository.findAll()).thenReturn(stocks);
-
-        // Call the retrieveAllStock method of the stockService
-        List<Stock> retrievedStocks = stockService.retrieveAllStock();
-
-        // Verify that the size of the retrievedStocks list is equal to the size of the stocks list
-        assertEquals(stocks.size(), retrievedStocks.size());
-
-
-
-        // Verify that the findAll method of the stockRepository is called exactly once
-        verify(stockRepository, times(1)).findAll();
+        assertTrue(exception.getMessage().contains("Stock Not Found with ID"));
     }
 
-    // No need to test other methods as requested
+
+    @Test
+    void testCreateUniversite() {
+        Stock universite = new Stock(12, "john",new HashSet<>());
+        iUniversiteService.addStock(universite);
+        verify(universiteRepository, times(1)).save(universite);
+        ArgumentCaptor<Stock> universiteArgumentCaptor = ArgumentCaptor.forClass(Stock.class);
+        verify(universiteRepository).save(universiteArgumentCaptor.capture());
+        Stock universiteCreated = universiteArgumentCaptor.getValue();
+        assertNotNull(universiteCreated.getIdStock());
+        assertEquals("john", universiteCreated.getTitle());
+    }
+
+
+
 
 }
