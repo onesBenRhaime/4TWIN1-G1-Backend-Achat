6fd09fa982cef16839575500d4e7ed43fda9f5d0
salahmarabou <salah.marabou@esprit.tn>
2024-04-12 17:59:35 +0100

    vvvv

--- src/main/java/tn/esprit/devops_project/services/ProductServiceImpl.java
+++ src/main/java/tn/esprit/devops_project/services/ProductServiceImpl.java
@@ -17,8 +17,8 @@ import java.util.List;
 @Slf4j
 public class ProductServiceImpl implements IProductService {
 
-   final ProductRepository productRepository;
-   final StockRepository stockRepository;
+   public ProductRepository productRepository;
+   public StockRepository stockRepository;
 
     @Override
     public Product addProduct(Product product, Long idStock) {
